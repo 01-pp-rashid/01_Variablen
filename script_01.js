fe4c6c48aeb41a63b228fdf94fae514136b3c7ae
rashid <r.sinouh1@gmail.com>
2023-01-15 00:27:36 +0100

    Programmierung 1

--- script_01.js
+++ script_01.js
@@ -9,32 +9,15 @@
 */
 
 // Konsolen-Ausgaben
-// console.log("Hallo");
-// console.log(firstName);
+//  console.log("Hallo");
+//  console.log(firstName);
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
-// let firstName; // Deklaration (Definition)
-// firstName = "Max"; // Wertzuweisung
-// console.log(firstName); // Ausgabe
+let firstName; // Deklaration 
+firstName = "Max" //Wertzuweisung 
+console.log(firstName); //Ausgabe
 
-// let familyName = "Mütze";
-// console.log(familyName);
-// console.log(firstName + " " + familyName);
-
-/* JS ist eine untypisierte Sprache! | untyped */
-
-// let test; 
-// test = "hallo";
-// test = 2;
-// test = true;
-
-// console.log("Datenyp: " + typeof test);
-// console.log("Inhalt: " + test);
-
-/***** 03a Konstanten *******/
-
-const test = "hallo";  // Variable mit KONSTANTEM Inhalt
-test = "hi"; // KEINE neue Zuweisung zur LZ möglich! --> Fehler
-console.log("Inhalt: " + test); // Ausgabe
-
+let familyname = "mütze";
+console.log(familyname);
+console.log(firstName +" "+  familyname);
